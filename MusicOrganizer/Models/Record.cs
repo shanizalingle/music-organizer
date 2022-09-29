a40aae8d5a72c5a026dfe78a32fbb39f64833e11
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-29 09:19:41 -0700

    add record.cs model
    Co-authored by:  Samuel Majerus <ladolego@gmail.com>
    Co-authored-by: Tiberius Lockett <tlockett680@gmail.com>

---
+++ MusicOrganizer/Models/Record.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Models
+{
+  public class Record
+  {
+    public string RecordTitle { get; set; }
+    public string ArtistName{ get; set; }
+    public int Id { get; }
+
+    private static List<Record> _records = new List<Record> {};
+
+    public Record(string recordTitle, string artistName)
+    {
+      RecordTitle = recordTitle;
+      ArtistName = artistName;
+      Id = _records.Count;
+      _records.Add(this);
+    }
+    public static List<Record> GetAll()
+    {
+      return _records;
+    }
+    public static void ClearAll()
+    {
+      _records.Clear();
+    }
+    public static Record Find(int searchId)
+    {
+      return _records[searchId-1];
+    }
+  }
+}
